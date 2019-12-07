c45530e9391f8ce7f8c9f836de91e3c77bc61736
loardjavad <javad.farmhc@gmail.com>
2019-12-07 21:58:36 +0330

    add sign in page and cmponent

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (<div>
+    <Header/>
     <Switch>
       <Route exact path="/" component={HomePage}/>
       <Route exact path="/shop" component={ShopPage}/>
+      <Route exact path='/signin' component={SignInAndSignUpPage}/>
     </Switch>
   </div>);
 }
